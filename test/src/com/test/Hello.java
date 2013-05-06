198652fd0d2c067a8e9d8b8a635cc95a101e6ebc
shon <hongsamu@gmail.com>
2013-05-07 05:53:35 +0800

    test 1

---
+++ test/src/com/test/Hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author Shon
+ * @since 2013-5-6 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("Hello GitHub !!!");
+
+	}
+
+}
